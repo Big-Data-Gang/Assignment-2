cb8d24e662f5b8ee1d65f2997a0097f6379cc0b0
sach-12 <sachinshankar2010@gmail.com>
2021-10-16 13:01:37 +0530

    finished T1

--- T1/mapper.py
+++ T1/mapper.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        incoming, outgoing = line.split('\t', 1)
+        try:
+            incoming = int(incoming)
+            outgoing = int(outgoing)
+        except ValueError:
+            continue
+        line = f"{incoming}\t{outgoing}"
+        print(line)
--- T1/reducer.py
+++ T1/reducer.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+
+import sys
+
+curr_node = None
+op_list = []
+fname = sys.argv[1]
+# f1 = open("adj_list.txt", "a")
+f2 = open(fname, "a")
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        line = line.strip()
+
+        incoming, outgoing = line.split('\t', 1)
+
+        try:
+            incoming = int(incoming.strip())
+            outgoing = int(outgoing.strip())
+        except ValueError:
+            continue
+
+        if curr_node == incoming:
+            if outgoing not in op_list:
+                op_list.append(outgoing)
+            else:
+                continue
+
+        else:
+            if curr_node != None:
+                print(f"{curr_node}\t{op_list}")
+                f2.write(f"{curr_node},1\n")
+            curr_node = incoming
+            op_list = []
+            op_list.append(outgoing)
+
+    if curr_node == incoming:
+        print(f"{curr_node}\t{op_list}")
+        f2.write(f"{curr_node},1\n")
+    f2.close()
